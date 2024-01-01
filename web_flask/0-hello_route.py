4c1e080d1a95bc2bc48b64dba18ee76fc2d29a3c
HoussineTabi <0620036304ht@gmail.com>
2024-01-01 13:30:45 +0000

    task_0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,8 @@ The hello flask model
 
 import flask
 app = flask.Flask(__name__)
-@app.route('/')
+@app.route('/',strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
