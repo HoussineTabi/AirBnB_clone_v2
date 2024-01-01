9ff02955eac67e562cb5f3f004c589f45f4718b2
HoussineTabi <0620036304ht@gmail.com>
2024-01-01 14:40:48 +0000

    tasks

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script flask application displays HBNB
+"""
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text=None):
+    if '_' in text:
+        text.replace('_', ' ')
+    return "C {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
