2d164f6a19a80820eb4bbdfabbfc18b515d10542
HoussineTabi <0620036304ht@gmail.com>
2024-01-01 15:07:53 +0000

    tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
-"""
-Script flask application displays HBNB
-"""
+""" Script that starts a Flask web application """
 from flask import Flask
-from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    default_route
+    """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display():
+def hbnb_route():
+    """
+    hbnb_route
+    """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text=None):
-    if '_' in text:
-        text.replace('_', ' ')
-    return "C {}".format(text)
+def c_route(text):
+    """
+    c_route
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """ python_route """
+    return "Python {}".format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
