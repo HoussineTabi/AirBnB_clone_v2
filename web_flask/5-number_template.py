cfcce98897ac236d8b5615bf743d1ba1377e5d24
HoussineTabi <0620036304ht@gmail.com>
2024-01-01 16:16:12 +0000

    task_5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,43 +5,33 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """
-    default_route
-    """
+def default_route():
+    """ default_route """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
-    """
-    hbnb_route
-    """
+    """hbnb_route """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    """
-    c_route
-    """
+    """ c_route """
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text="is cool"):
-    """
-    python_route
-    """
+    """ python_route """
     return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
-    """
-    number_route
-    """
+    """ number_route """
     if isinstance(n, int):
         return "{} is a number".format(n)
 
@@ -53,5 +43,16 @@ def number_template_route(n=None):
         return render_template("5-number.html", n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even_route(n=None):
+    """ number_odd_or_even_route """
+    if isinstance(n, int):
+        if n % 2:
+            eo = "odd"
+        else:
+            eo = "even"
+        return render_template("6-number_odd_or_even.html", n=n, eo=eo)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run()
